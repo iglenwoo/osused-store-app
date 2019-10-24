f8750c934866e4ca8d57ab6be615038a4ee3ff19
Ingyu Woo <iglenwoo@gmail.com>
2019-10-23 22:28:25 -0700

    fetch item list

--- src/pages/ItemListPage/index.js
+++ src/pages/ItemListPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ItemList } from '../../components/ItemList'
 import './ItemListPage.css'
 
@@ -7,15 +7,30 @@ import mockItems from './ItemList.mock'
 export const ItemListPage = () => {
   const [items, setItems] = useState(mockItems)
 
+  useEffect(() => {
+    async function fetchItems() {
+      const response = await fetch('http://localhost:4000/items', {
+        method: 'GET',
+      })
+      const items = await response.json()
+      setItems(items)
+    }
+    fetchItems().catch(e => console.error(e))
+  }, [])
+
   return (
     <>
       <div className="items-labels">
         <h1>Products for you</h1>
         <h2>Recently Posted</h2>
       </div>
-      <div class="item-gallery">
-        <div class="item-elements">
-          <ItemList items={items} />
+      <div className="item-gallery">
+        <div className="item-elements">
+          {items.length ? (
+            <ItemList items={items} />
+          ) : (
+            <h2>No items currently...</h2>
+          )}
         </div>
       </div>
     </>
