e2a3fe22d9a07ef8a19f2757fd243882cbe1c8f4
Ingyu Woo <iglenwoo@gmail.com>
2019-10-31 12:35:07 -0700

    fix tests for useHistory

--- src/pages/Login/Login.test.js
+++ src/pages/Login/Login.test.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { Login } from './index'
 
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: jest.fn(),
+  }),
+}))
+
 it('renders without crashing', () => {
-  //shallow(<Login />)
+  shallow(<Login />)
 })
--- src/pages/Signup/Signup.test.js
+++ src/pages/Signup/Signup.test.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { Signup } from './index'
 
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: jest.fn(),
+  }),
+}))
+
 it('renders without crashing', () => {
   shallow(<Signup />)
 })
